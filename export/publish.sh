3f606aad073ae22b8f1e1b83c4bb0ab9ff2e4ea8
Zetsubou <3969837+koroshiya@users.noreply.github.com>
2020-06-19 19:14:53 -0400

    Rebuild

--- export/publish.sh
+++ export/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#rm -rf 192.168.122.196/.git
-#cp -r git 192.168.122.196/.git
-cd 192.168.122.196
+#rm -rf 192.168.123.196/.git
+#cp -r git 192.168.123.196/.git
+cd 192.168.123.196
 git add .
 git commit -m "Rebuild"
 git push
