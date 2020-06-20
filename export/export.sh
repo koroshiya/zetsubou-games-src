adde31d7ae0485a26cc872ac99a0a1ca97c42904
Zetsubou <3969837+koroshiya@users.noreply.github.com>
2020-06-20 05:40:05 -0400

    Rebuild

--- export/export.sh
+++ export/export.sh
@@ -1,10 +1,14 @@
-rm -rf 192.168.123.196/*
-wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains 192.168.123.196      --no-parent --no-check-certificate  http://192.168.123.196/
-echo "zetsubou.games" > 192.168.123.196/CNAME
-find ./192.168.123.196/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
-rm ./192.168.123.196/rss/index.html
-curl -k http://192.168.123.196/rss/ > ./192.168.123.196/rss/feed.xml
-find ./192.168.123.196/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
-find ./192.168.123.196/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
-find ./192.168.123.196/ -type d -name "amp" -exec rm -rf "{}" \;
-find ./192.168.123.196/ -type f -print0 | xargs -0 sed -i 's/http:\/\/192\.168\.123\.196/https:\/\/zetsubou\.games/g'
+mv 127.0.0.1/.git tmpgit
+rm -rf 127.0.0.1/*
+wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains 127.0.0.1      --no-parent --no-check-certificate  http://127.0.0.1/
+echo "zetsubou.games" > 127.0.0.1/CNAME
+find ./127.0.0.1/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
+mkdir ./127.0.0.1/rss/
+rm ./127.0.0.1/rss/index.html
+curl -k http://127.0.0.1/rss/ > ./127.0.0.1/rss/feed.xml
+find ./127.0.0.1/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
+find ./127.0.0.1/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
+find ./127.0.0.1/ -type d -name "amp" -exec rm -rf "{}" \;
+find ./127.0.0.1/ -type f -print0 | xargs -0 sed -i 's/http:\/\/127\.0\.0\.1//g'
+find ./127.0.0.1/ -type f -print0 | xargs -0 sed -i 's/\.\.\//\//g'
+mv tmpgit 127.0.0.1/.git
--- export/publish.sh
+++ export/publish.sh
@@ -2,7 +2,7 @@
 
 #rm -rf 192.168.123.196/.git
 #cp -r git 192.168.123.196/.git
-cd 192.168.123.196
+cd 127.0.0.1
 git add .
 git commit -m "Rebuild"
 git push
