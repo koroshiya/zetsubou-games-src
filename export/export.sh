8a33bd12b7e3807b50fac3eb6f4a2a35a4968142
Zetsubou <3969837+koroshiya@users.noreply.github.com>
2021-09-13 05:54:39 +0000

    Rebuild

--- export/export.sh
+++ export/export.sh
@@ -1,15 +1,15 @@
-mv 127.0.0.1/.git tmpgit
-rm -rf 127.0.0.1/*
-wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains 127.0.0.1      --no-parent --no-check-certificate  http://127.0.0.1/
-echo "zetsubou.games" > 127.0.0.1/CNAME
-find ./127.0.0.1/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
-mkdir ./127.0.0.1/rss/
-rm ./127.0.0.1/rss/index.html
-curl -k http://127.0.0.1/rss/ > ./127.0.0.1/rss/feed.xml
-find ./127.0.0.1/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
-find ./127.0.0.1/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
-find ./127.0.0.1/ -type d -name "amp" -exec rm -rf "{}" \;
-find ./127.0.0.1/ -type f -print0 | xargs -0 sed -i 's/http:\/\/127\.0\.0\.1//g'
-#find ./127.0.0.1/ -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\//\//g'
-#find ./127.0.0.1/ -type f -print0 | xargs -0 sed -i 's/\.\.\//\//g'
-mv tmpgit 127.0.0.1/.git
+mv ghost.test/.git tmpgit
+rm -rf ghost.test/*
+wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains ghost.test      --no-parent --no-check-certificate  http://ghost.test/
+echo "zetsubou.games" > ghost.test/CNAME
+find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
+mkdir ./ghost.test/rss/
+rm ./ghost.test/rss/index.html
+curl -k http://ghost.test/rss/ > ./ghost.test/rss/feed.xml
+find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
+find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
+find ./ghost.test/ -type d -name "amp" -exec rm -rf "{}" \;
+find ./ghost.test/ -type f -print0 | xargs -0 sed -i 's/http:\/\/ghost\.test//g'
+#find ./ghost.test/ -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\//\//g'
+#find ./ghost.test/ -type f -print0 | xargs -0 sed -i 's/\.\.\//\//g'
+mv tmpgit ghost.test/.git
--- export/publish.sh
+++ export/publish.sh
@@ -2,7 +2,7 @@
 
 #rm -rf 192.168.123.196/.git
 #cp -r git 192.168.123.196/.git
-cd 127.0.0.1
+cd ghost.test
 git add .
 git commit -m "Rebuild"
 git push
