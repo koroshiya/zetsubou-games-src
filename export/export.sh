8f1d2360cf59f202253d76c26b9d7cc136598a84
Zetsubou <3969837+koroshiya@users.noreply.github.com>
2021-09-14 01:07:34 +0000

    Rebuild

--- export/export.sh
+++ export/export.sh
@@ -1,15 +1,16 @@
-mv ghost.test/.git tmpgit
-rm -rf ghost.test/*
-wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains ghost.test      --no-parent --no-check-certificate  http://ghost.test/
-echo "zetsubou.games" > ghost.test/CNAME
-find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
-mkdir ./ghost.test/rss/
-rm ./ghost.test/rss/index.html
-curl -k http://ghost.test/rss/ > ./ghost.test/rss/feed.xml
-find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
-find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
-find ./ghost.test/ -type d -name "amp" -exec rm -rf "{}" \;
-find ./ghost.test/ -type f -print0 | xargs -0 sed -i 's/http:\/\/ghost\.test//g'
-#find ./ghost.test/ -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\//\//g'
-#find ./ghost.test/ -type f -print0 | xargs -0 sed -i 's/\.\.\//\//g'
-mv tmpgit ghost.test/.git
+mv zetsubou.games/.git tmpgit
+rm -rf zetsubou.games/*
+#wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains zetsubou.games      --no-parent --no-check-certificate  https://zetsubou.games/
+wget --recursive --page-requisites --html-extension --restrict-file-names=windows --domains zetsubou.games --no-parent --no-check-certificate https://zetsubou.games/
+echo "zetsubou.games" > zetsubou.games/CNAME
+#find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
+mkdir ./zetsubou.games/rss/
+rm ./zetsubou.games/rss/index.html
+curl -k https://zetsubou.games/rss/ > ./zetsubou.games/rss/feed.xml
+find ./zetsubou.games/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
+find ./zetsubou.games/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
+find ./zetsubou.games/ -type d -name "amp" -exec rm -rf "{}" \;
+#find ./zetsubou.games/ -type f -print0 | xargs -0 sed -i 's/http:\/\/ghost\.test//g'
+#find ./zetsubou.games/ -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\//\//g'
+#find ./zetsubou.games/ -type f -print0 | xargs -0 sed -i 's/\.\.\//\//g'
+mv tmpgit zetsubou.games/.git
