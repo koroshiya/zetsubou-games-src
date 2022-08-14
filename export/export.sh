b9a9997f70233fa6ee9eff2556f94cff1b6ceb45
Zetsubou <3969837+koroshiya@users.noreply.github.com>
2022-08-14 04:42:56 +0000

    Rebuild

--- export/export.sh
+++ export/export.sh
@@ -1,16 +1,13 @@
 mv zetsubou.games/.git tmpgit
 rm -rf zetsubou.games/*
-#wget      --recursive           --page-requisites      --html-extension      --convert-links    --restrict-file-names=windows        --domains zetsubou.games      --no-parent --no-check-certificate  https://zetsubou.games/
+
 wget --recursive --page-requisites --html-extension --restrict-file-names=windows --domains zetsubou.games --no-parent --no-check-certificate https://zetsubou.games/
 echo "zetsubou.games" > zetsubou.games/CNAME
-#find ./ghost.test/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/.jpg\S*.jpg/.jpg/g'
-mkdir ./zetsubou.games/rss/
-rm ./zetsubou.games/rss/index.html
-curl -k https://zetsubou.games/rss/ > ./zetsubou.games/rss/feed.xml
-find ./zetsubou.games/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/index.html/rss\/feed.xml/g'
-find ./zetsubou.games/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/rss\/" rel="self"/rss\/feed.xml" rel="self"/g'
-find ./zetsubou.games/ -type d -name "amp" -exec rm -rf "{}" \;
-#find ./zetsubou.games/ -type f -print0 | xargs -0 sed -i 's/http:\/\/ghost\.test//g'
-#find ./zetsubou.games/ -type f -print0 | xargs -0 sed -i 's/\.\.\/\.\.\//\//g'
-#find ./zetsubou.games/ -type f -print0 | xargs -0 sed -i 's/\.\.\//\//g'
+
+mv zetsubou.games/assets/built/casper.js* zetsubou.games/assets/built/casper.js
+mv zetsubou.games/assets/built/screen.css@v=32a9c065be.css zetsubou.games/assets/built/screen.css
+mv zetsubou.games/public/cards.min.css* zetsubou.games/public/cards.min.css
+mv zetsubou.games/public/cards.min.js* zetsubou.games/public/cards.min.js
+mv zetsubou.games/public/comment-counts.min.js* zetsubou.games/public/comment-counts.min.js
+
 mv tmpgit zetsubou.games/.git
--- export/publish.sh
+++ export/publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#rm -rf 192.168.123.196/.git
-#cp -r git 192.168.123.196/.git
 cd zetsubou.games
 git add .
 git commit -m "Rebuild"
